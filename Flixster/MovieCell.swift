715c92b0945446c484abf5873c679ebdfe2ca004
Lauren Work <workla@oregonstate.edu>
2021-09-23 22:46:43 -0700

    continue flix app and launch screen logo showing

---
+++ Flixster/MovieCell.swift
@@ -0,0 +1,23 @@
+//
+//  MovieCell.swift
+//  Flixster
+//
+//  Created by Lauren Work on 9/25/21.
+//
+
+import UIKit
+
+class MovieCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
---
+++ Flixster/MoviesViewController.swift
@@ -0,0 +1,8 @@
+//
+//  MoviesViewController.swift
+//  Flixster
+//
+//  Created by Lauren Work on 9/24/21.
+//
+
+import Foundation
