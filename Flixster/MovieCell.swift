4d8e732142bb55e3d623827f45418b148ba4c529
Lauren Work <workla@oregonstate.edu>
2021-09-25 14:41:08 -0700

    Add tableview with data from api and make constraints

--- Flixster/MovieCell.swift
+++ Flixster/MovieCell.swift
@@ -9,6 +9,13 @@ import UIKit
 
 class MovieCell: UITableViewCell {
 
+    @IBOutlet weak var titleLabel: UILabel!
+    
+    @IBOutlet weak var synopsisLabel: UILabel!
+    
+    @IBOutlet weak var posterImage: UIImageView!
+    
+    
     override func awakeFromNib() {
         super.awakeFromNib()
         // Initialization code
