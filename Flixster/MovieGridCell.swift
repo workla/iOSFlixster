a2d96d87f44f7d5d63f1629591a9f1807160e7ee
Lauren Work <workla@oregonstate.edu>
2021-09-27 22:48:23 -0700

    Start gridview for superhero

---
+++ Flixster/MovieGridCell.swift
@@ -0,0 +1,13 @@
+//
+//  MovieGridCell.swift
+//  Flixster
+//
+//  Created by Lauren Work on 9/27/21.
+//
+
+import UIKit
+
+class MovieGridCell: UICollectionViewCell {
+    
+    @IBOutlet weak var posterView: UIImageView!
+}
